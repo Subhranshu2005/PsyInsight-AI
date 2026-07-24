209403e28437019586c9a7542ea4fed9d48d801b
Subhranshu Ranjan Sahoo <subhranshuranjansahoo2004@gmail.com>
2026-07-24 10:51:04 +0530

    Start DataLoader class

--- src/psyinsight/preprocessing/loader.py
+++ src/psyinsight/preprocessing/loader.py
@@ -1,1 +1,16 @@
+"""
+PsyInsight AI
+Data Loader Module
 
+Author: Subhranshu Ranjan Sahoo
+
+This module loads datasets from different file formats.
+"""
+
+from pathlib import Path
+
+import pandas as pd
+class DataLoader:
+    """
+    A class to load datasets into pandas DataFrames.
+    """
