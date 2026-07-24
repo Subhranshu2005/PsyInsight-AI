2468172a4a8cec0e0de97cda0201a8ecc54b07ea
Subhranshu Ranjan Sahoo <subhranshuranjansahoo2004@gmail.com>
2026-07-24 11:41:46 +0530

    Implement ManualProbability class for distributions
    Add ManualProbability class with normal_pdf method stub

--- src/psyinsight/probability/manual.py
+++ src/psyinsight/probability/manual.py
@@ -1,1 +1,24 @@
+"""
+PsyInsight AI
+Manual Probability Module
 
+Author: Subhranshu Ranjan Sahoo
+
+This module implements probability concepts from scratch.
+"""
+
+import math
+
+
+class ManualProbability:
+    """
+    Manual implementations of probability distributions.
+    """
+
+    @staticmethod
+    def normal_pdf(x, mean, std):
+        """
+        Compute the Probability Density Function (PDF)
+        of the Normal Distribution.
+        """
+        pass
